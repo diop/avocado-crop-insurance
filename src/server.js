8aa66a4e5cdd3a59e5b8c44fff889e1a06a8b976
diop <fodesdiop@gmail.com>
2018-01-20 16:17:08 -0800

    Running server

--- src/server.js
+++ src/server.js
@@ -9,9 +9,9 @@ app.set('views', 'src/views')
 
 app.use(express.static('src/public'))
 
-app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use('/', index)
+// app.use('/', index)
 
 app.listen(port, () => {
   console.log('Listening for parties on port:', port)
