4f47bfdfe32eccb0c8ed8c0cb552003405c08542
jason00111 <emberleyjason@gmail.com>
2018-01-21 00:55:33 -0800

    updates crop failure function

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,16 +1,26 @@
 const { getActiveFarmersEthAdd } = require('../database/queries')
 const { getWeatherInfo } = require('../api')
 
+const maxSafeAvocadoTemp = 311
+
 const checkForCropFailure = () => {
-  // call api
-  // if temp above *
-  getActiveFarmersEthAdd()
-    .then(addresses => {
-      addresses.forEach(address => {
-        // call contract pay method
-      })
+
+  getWeatherInfo()
+    .then(weatherInfo => {
+      const maxTemp = weatherInfo.main.temp_max
+
+      if (maxTemp >= maxSafeAvocadoTemp) {
+        getActiveFarmersEthAdd()
+          .then(addresses => {
+            addresses.forEach(address => {
+              // call contract pay method
+            })
+          })
+          .catch(console.error)
+      }
     })
     .catch(console.error)
+
 }
 
 module.exports = { checkForCropFailure }
