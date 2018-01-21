1d7ab3cece0c783d15fede55b2035dc567da3cd8
diop <fodesdiop@gmail.com>
2018-01-20 19:21:55 -0800

    Add safe math

--- src/contracts/AvocadoPolicy.sol
+++ src/contracts/AvocadoPolicy.sol
@@ -1,6 +1,36 @@
 pragma solidity ^0.4.18;
 
+//import "./SafeMath.sol";
+
+library SafeMath {
+  function mul(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a * b;
+    assert(a == 0 || c / a == b);
+    return c;
+  }
+
+  function div(uint256 a, uint256 b) internal constant returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+
+  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  function add(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+
+
 contract AvocadoPolicy {
+  using SafeMath for uint256;
   address owner;
   uint256 ownerAccount;
   mapping (address => uint256) policyAccount;
