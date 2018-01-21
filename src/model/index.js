a009df05bbe4c9ed4c2b7d16568434aaf871d03e
Wanlin Zheng <jzwlin@WanlinmatoMacBook-Pro.local>
2018-01-21 01:51:08 -0800

    add model

---
+++ src/model/index.js
@@ -0,0 +1,9 @@
+function getProb(week,minTemp,rainfall,sunlightExposure,elevation,experience,size_of_farm,technique,disease){
+var probSuccess= Math.exp(0.1244899+ 0.0009378637*week+0.0003744942*minTemp+0.002816007*rainfall
+                          +0.003786983*sunlightExposure+0.000004729335*elevation+0.0009401107*experience
+                          +0.0004923823*size_of_farm+0.02829916*technique+0.02829434*disease)/
+                          (1+Math.exp(0.1244899+ 0.0009378637*week+0.0003744942*minTemp+0.002816007*rainfall
+                                                    +0.003786983*sunlightExposure+0.000004729335*elevation+0.0009401107*experience
+                                                    +0.0004923823*size_of_farm+0.02829916*technique+0.02829434*disease))
+return probSuccess;
+}
