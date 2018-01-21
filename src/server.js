5d517312a746de3709eb60cea1c285ded01cb782
jason00111 <emberleyjason@gmail.com>
2018-01-20 23:31:00 -0800

    adds node-scheduler

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
 const express = require('express')
-const app = express()
 const bodyParser = require('body-parser')
+const schedule = require('node-schedule')
 const index = require('./routes/index')
 
+const app = express()
+
 const port = process.env.PORT || 3000
+
+// schedule.scheduleJob('23 * * *', callApi())
 
 app.set('view engine', 'ejs')
 app.set('views', 'src/views')
