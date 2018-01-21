3502ca5dcb6ff52ff5b2a0066b8fdabe78a500ac
diop <fodesdiop@gmail.com>
2018-01-20 16:10:48 -0800

    Add basic express server

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.set('view engine', 'ejs')
+app.set('views', 'src/views')
+
+app.use(express.static('src/public'))
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use('/', index)
+
+app.listen(port, () => {
+  console.log('Listening for parties on port:', port)
+})
+
+module.exports = app
